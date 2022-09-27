6c0d2bfba1bd7362d872e408e79d08ba33e4d0c0
Dimitris Niras <niras.94@gmail.com>
2022-09-27 15:57:38 +0300

    Recording using split

---
+++ build/precache-manifest.5158aed03984366bb2dec847d90294b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322a83a24e176654a755f0e633c39ac6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cc1cc6cfea174f7db36",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "40d5d870967dec6d20a5",
+    "url": "/static/js/2.fb39c600.chunk.js"
+  },
+  {
+    "revision": "d7bb0c86a300f0c4b390b317f27060c1",
+    "url": "/static/js/2.fb39c600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cc1cc6cfea174f7db36",
+    "url": "/static/js/main.52efa7b8.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.52efa7b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "296b44381a5b88bfb39d",
+    "url": "/static/js/runtime-main.2abf9b54.js"
+  }
+]);
